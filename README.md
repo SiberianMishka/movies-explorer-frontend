cb0c59a9a2ca16d0dc556e11a8f32b248327f1c2
Ivan Stremaus <i.stremaus@yandex.ru>
2023-07-19 08:38:14 +0600

    l-2: add folders

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+# Дипломная работа "MoviesExplorer"
+
 # movies-explorer-frontend
+
+Репозиторий для проектной работы `MoviesExplorer`, включающий фронтенд часть приложения со следующими возможностями: ...
+
+Адрес репозитория: [https://github.com/SiberianMishka/movies-explorer-frontend]
+
+## Ссылки на проект
+
+Ссылка на макет: https://disk.yandex.ru/d/EdmbZe7Nu0ijSg
+
+IP-адрес 84.252.140.69
+
+Backend https://stremaus.nomoredomains.xyz
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+
+import Promo from '../Promo/Promo.jsx';
+import AboutProject from '../AboutProject/AboutProject.jsx';
+import Techs from '../Techs/Techs.jsx';
+import AboutMe from '../AboutMe/AboutMe.jsx';
+import Portfolio from '../Portfolio/Portfolio.jsx';
+
+// Promo — компонент с вёрсткой баннера страницы «О проекте».
+// NavTab — компонент с навигацией по странице «О проекте».
+// AboutProject — компонент с описанием дипломного проекта.
+// Techs — компонент с использованными технологиями.
+// AboutMe — компонент с информацией о студенте.
+// Portfolio — компонент со ссылками на другие проекты.
